f1d361675a19d51f505d7f52278196865204b353
emm7494 <emm7494@gmail.com>
2019-09-17 02:53:05 +0000

    Added form render_field macro

--- quiz_hound/views/auth.py
+++ quiz_hound/views/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, flash, redirect, render_template, url_for
 from ..forms import auth
 from flask import Blueprint
 
@@ -7,6 +7,10 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
-    form = auth.LoginForm()
     title = 'Sign In'
+    form = auth.LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f'Login successful with username: {form.username.data} and remember_me: {form.remember_me.data}')
+        return redirect(url_for('index.index'))
     return render_template('auth/login.html', title=title, form=form)
